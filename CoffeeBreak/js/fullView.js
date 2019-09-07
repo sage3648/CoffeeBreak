a0ec49224dcfa87349448c60fb7ef7379b55ef3f
Sage Stainsby <sage.stainsbys@gmail.com>
2019-09-07 22:18:44 +1000

    Mock events and small changes to layout

--- CoffeeBreak/js/fullView.js
+++ CoffeeBreak/js/fullView.js
@@ -5,8 +5,30 @@ document.addEventListener('DOMContentLoaded', function() {
         plugins: [ 'timeGrid' ],
         events: [
             {
-                title  : 'Event Test',
-                start  : '2019-09-05T12:30:00',
+                title  : 'Lunch with Molly!',
+                start  : '2019-09-06T12:00:00',
+                end  : '2019-09-06T13:30:00',
+
+                allDay : false // will make the time show
+            },
+            {
+                title  : 'Breakfast with Izy!',
+                start  : '2019-09-07T09:15:00',
+                end  : '2019-09-07T09:16:00',
+
+                allDay : false // will make the time show
+            },
+            {
+                title  : 'Breakfast with Chuksy',
+                start  : '2019-09-08T12:30:00',
+                end  : '2019-09-08T14:30:00',
+
+                allDay : false // will make the time show
+            }, {
+                title  : 'Dinner with Jagmeet!',
+                start  : '2019-09-05T17:30:00',
+                end  : '2019-09-05T19:30:00',
+
                 allDay : false // will make the time show
             }
         ]
