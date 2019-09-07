dc5fbe12d5c70cf63b87c58ae9cb6ba50ca54d84
Sage Stainsby <sage.stainsbys@gmail.com>
2019-09-07 18:14:43 +1000

    small changes

---
+++ CoffeeBreak/js/options.js
@@ -0,0 +1,38 @@
+document.getElementById('save').addEventListener('click', save);
+document.getElementById('15min').addEventListener('click', set15);
+document.getElementById('30min').addEventListener('click', set30);
+document.addEventListener('DOMContentLoaded', load );
+
+var current = document.getElementById('current');
+
+    function set15() {
+        current.textContent = "Current frequency set: 15 Minutes";
+        save();
+    }
+
+    function set30() {
+        current.textContent = "Current frequency set: 30 Minutes";
+        save();
+    }
+    //load currently assigned options
+     function load() {
+         chrome.storage.sync.get(['saveFreq'], function (result) {
+             if (load) {
+                 current.textContent = "" + result.saveFreq;
+             } else {
+                 current.textContent = "" + result.saveFreq;
+             }
+         });
+     }
+
+     //save currently assigned options
+     function save() {
+         chrome.storage.sync.set({saveFreq:current.textContent}, function() {
+            // console.log('Value is set to ' + value);
+         })
+
+
+
+
+
+     }
