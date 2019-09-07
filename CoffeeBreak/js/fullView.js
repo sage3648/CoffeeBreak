f4aedb82ad8f6ee9a29ccbd0fd3c427bca0c72b8
Sage Stainsby <sage.stainsbys@gmail.com>
2019-09-08 00:44:40 +1000

    Layout Updates

--- CoffeeBreak/js/fullView.js
+++ CoffeeBreak/js/fullView.js
@@ -3,33 +3,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
         plugins: [ 'timeGrid' ],
+        eventBorderColor: 'black',
+        views: {
+            timeGrid: {
+                type: 'timeGridDay',
+                duration: {days: 1},
+                buttonText: '1 day'
+            }
+        },
+        businessHours: true,
+        allDaySlot: false,
+
         events: [
             {
                 title  : 'Lunch with Molly!',
                 start  : '2019-09-06T12:00:00',
                 end  : '2019-09-06T13:30:00',
 
-                allDay : false // will make the time show
             },
             {
                 title  : 'Breakfast with Izy!',
                 start  : '2019-09-07T09:15:00',
                 end  : '2019-09-07T09:16:00',
 
-                allDay : false // will make the time show
             },
             {
                 title  : 'Breakfast with Chuksy',
                 start  : '2019-09-08T12:30:00',
                 end  : '2019-09-08T14:30:00',
 
-                allDay : false // will make the time show
             }, {
                 title  : 'Dinner with Jagmeet!',
                 start  : '2019-09-05T17:30:00',
                 end  : '2019-09-05T19:30:00',
 
-                allDay : false // will make the time show
             }
         ]
     });
