560ad32424def5b7f79d227633cc0d482e1e49e2
Sage Stainsby <sage.stainsbys@gmail.com>
2019-09-07 21:52:37 +1000

    simple layout

--- CoffeeBreak/js/fullView.js
+++ CoffeeBreak/js/fullView.js
@@ -2,7 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: [ 'dayGrid' ]
+        plugins: [ 'timeGrid' ],
+        events: [
+            {
+                title  : 'Event Test',
+                start  : '2019-09-05T12:30:00',
+                allDay : false // will make the time show
+            }
+        ]
     });
 
     calendar.render();
